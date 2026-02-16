57964527c66621fcde28c83bfb55c2c70effb9b9
diego.fontes <diego.fontes@artesanalinvestimentos.com.br>
2026-02-16 14:33:08 -0300

    remove ruby backend

--- backend/app/controllers/health_controller.rb
+++
@@ -1,5 +0,0 @@
-class HealthController < ApplicationController
-  def index
-    render json: { status: "ok", message: "Micro-SaaS API is running 🚀" }
-  end
-end
--- backend/test/controllers/health_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class HealthControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
