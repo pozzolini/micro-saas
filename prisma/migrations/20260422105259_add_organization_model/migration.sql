d31b72e75c20aa1acab248fb6f89393167b9d7db
Diego Pozzolini <pozzolini@yahoo.com>
2026-04-22 14:10:32 -0300

    add migrations

---
+++ prisma/migrations/20260422105259_add_organization_model/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "organizationId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "Organization" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Organization_slug_key" ON "Organization"("slug");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE SET NULL ON UPDATE CASCADE;
